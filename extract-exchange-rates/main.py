3c20b0d582e448c657026d6bcc903d5091d29d6b
Bruno Freitas <97988515+bruno-freitas-pro@users.noreply.github.com>
2023-02-07 17:51:56 -0300

    Update and rename extract-exchange-rates.py to extract-exchange-rates/main.py

--- extract-exchange-rates/main.py
+++ extract-exchange-rates/main.py
@@ -1,11 +1,8 @@
-#!mamba install pandas==1.3.3 -y
-#!mamba install requests==2.26.0 -y
-
 import requests
 import pandas as pd
 import json
 
-url = "https://api.apilayer.com/exchangerates_data/latest?base=EUR&apikey=kRFfp4wOHWQ9g9RLljUfIgM5NmJ9EAvv" #Make sure to change ******* to your API key.
+url = "https://api.apilayer.com/exchangerates_data/latest?base=EUR&apikey=*******" #Make sure to change ******* to your API key.
 query = requests.get(url)
 api = query.text
 
